1010703a905da44459e3bd218bd5870c5b4a9e63
Wakil17 <youssoufwakil@gmail.com>
2025-01-20 18:49:24 +0100

    brouillon parser

---
+++ Database/src/parser.c
@@ -0,0 +1,16 @@
+#include "parser.h"
+#include <string.h>
+
+Command parse_command(char* input) {
+    Command cmd = {CMD_ERROR};
+    
+    // TODO:
+    // 1. Séparer la ligne en mots
+    // 2. Si "insert", parser id, name, email
+    // 3. Si "select", juste type
+    // 4. Si "delete", parser id
+    // 5. Si "exit", type EXIT
+    // Objectif: Transformer texte en commande exécutable
+    
+    return cmd;
+}
