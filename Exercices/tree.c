8f52065b13a539d3c835fad9070a21a9a457f6ba
Wakil17 <youssoufwakil@gmail.com>
2025-01-20 14:14:33 +0100

    addnode new node ok

--- Exercices/tree.c
+++ Exercices/tree.c
@@ -21,55 +21,45 @@ Node*  newnode(int value){
        	new->right = NULL;
 
 	printf("Noeaud crée avec succès ! \n  Value: %d", new->value);	
-	return *new;
+	return new;
 }
 
 Node* addnode(Node* racine, int value){
-
-	if(racine->value == NULL){
-		newnode = newnode(value);
-		return newnode;
+	//si racine vide on crée une racine 
+	if(racine == NULL){
+		return newnode(value);
 	}
 
-	
+	//si la valeur et plus petite on va à gauche 
 	if(racine->value > value){
-		Node *new = malloc(sizeof(Node));
-	
-	if(new == NULL){
-	
+		racine->left = addnode(racine->left, value);
 	}
-	if(racine->value <= value){
-		Node *new = malloc(sizeof(Node));
-		
-		if(new == NULL){
-		printf("Erreur d\'allocation memoire \n");
-		return 0;
-		}
-
-		new->value = value;
-	       	new->left = NULL;
-		new->right = NULL;	
-		
-	} 
+	//sinon on va a droite 
+	else if (racine->value < value){
+		racine->right = addnode(racine->right, value);
+	}	
+	return racine;		
+}
 
 
-	if(new == NULL){
-		printf("Erreur d\'allocation memoire \n");
-		return 0;
-		}
-	}
+void inorder(Node* racine){
+
 
 }
 
 
 int main(){
 
-	Node* head = malloc(sizeof(Node))
+	Node* head = NULL;
 
-	if(head == NULL){
-		printf("Echec lors de la création du premier noeud \n");
-		return 0;
-	}
-	Node* new = newnode(head, 9);
-	return 0;
+	head = addnode(head, 5);
+	head = addnode(head, 3);
+	head = addnode(head, 7);
+	head = addnode(head, 1);
+	head = addnode(head, 9);
+	
+	inorder(head);
+	printf("\n");
+
+	return 0; 
 }
